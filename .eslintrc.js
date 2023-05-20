29868a3b7b353847310bc6942a465a480bb78cc4
arrowfieldfzp <1573488953@qq.com>
2023-05-20 09:49:44 +0800

    ant design pro

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,7 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
+  extends: [require.resolve('@umijs/lint/dist/config/eslint')],
+  globals: {
+    page: true,
+    REACT_APP_ENV: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": [
-      // 必须使用全驼峰名 两个单词的那种
-      "error",
-      {
-        ignores: ["Login"], //需要忽略的组件名
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // lintOnSave: false, // 必须使用全驼峰命名
-});
